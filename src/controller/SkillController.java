861dcc2cf9b654d777f7f6088786d16900415c81
Neilmarcel <71927366+Neilmarcel@users.noreply.github.com>
2020-11-19 23:20:35 +0530

    commit 5

--- src/controller/SkillController.java
+++ src/controller/SkillController.java
@@ -1,10 +1,52 @@
 package controller;
 
+import java.io.*;
+import java.sql.SQLException;
+import model.Skill;
+import java.util.List;
+import dao.ISkillDao;
+import daoImpl.SkillDaoImpl;
+
 public class SkillController {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+    
+	SkillDaoImpl skillDao=null;
+	public SkillController() throws ClassNotFoundException, SQLException {
+		skillDao=new SkillDaoImpl();
 	}
-
+	public void addSkill() {
+		Skill ski=new Skill();
+		try {
+			BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Enter Skill Name:");
+			ski.setSkillName(reader.readLine());
+			System.out.println("Enter Skill Description:");
+			ski.setSkillDescription(reader.readLine());
+			System.out.println("Enter UserId:");
+			emp.setUserId(reader.readLine());
+			System.out.println("Enter Password:");
+			emp.setPassword(reader.readLine());
+			System.out.println("Enter Role:");
+			String =reader.readLine();
+			emp.setRole(role);
+			System.out.println("Enter Gender:");
+			emp.setGender(reader.readLine());
+			if(role.equals("HRA")) {
+				emp.setActive("Active");
+			}
+			else {
+				emp.setActive("Deactive");
+			}
+			//Calling dao method for insert record
+		}
+		catch(IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	public void getAllSkills() {
+		List<Skill> allSkillList=ski.getAllSkills();
+		//Iterator itr=allEmpList.iterator();
+		for(Skill emp:allSkillList) {
+			System.out.println(ski);
+		}
+	}
 }
--- src/dao/ISkillDao.java
+++ src/dao/ISkillDao.java
@@ -1,5 +1,13 @@
 package dao;
+import java.util.List;
+import model.Skill;
 
 public interface ISkillDao {
 
+	List<Skill> getAllSkills();
+	void addSkill(Skill ski);
+	Skill getSkillById(int id);
+	void updateSkill(Skill ski);
+	void deactivateSkill(int id);
+	void deleteSkill(int id);
 }
